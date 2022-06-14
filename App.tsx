5571905babb093f891184d54e011317ce41df1fb
Oscar Mañas <oscarmanash@gmail.com>
2022-06-14 18:56:03 +0200

    amplify install

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Router from './navigation/Router';
+import { Amplify } from 'aws-amplify'
+import awsconfig from './aws-exports'
+Amplify.configure(awsconfig)
 
 export default function App() {
+  
   return (
     <SafeAreaProvider>
       <Router />
